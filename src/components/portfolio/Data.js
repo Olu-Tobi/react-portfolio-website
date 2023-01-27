981904624b6a9025560e2fa88cf026c4871cbec8
unknown <tobidaramola77@gmail.com>
2023-01-27 13:53:27 +0100

    lastest commit

--- src/components/portfolio/Data.js
+++ src/components/portfolio/Data.js
@@ -1,15 +1,24 @@
 
 export const Data = [
-        
+
         {
           "id": 1,
+          "image": 'https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/laxapp.png?alt=media&token=9889163b-28d8-448c-9c5b-8d4c734414fc',
+          "title": "Lax",
+          "github": "https://github.com/Olu-Tobi/mern_client",
+          "demo": "https://lax-steel.vercel.app/"
+        },
+        
+        {
+          "id": 2,
           "image": 'https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/kontenthome.png?alt=media&token=377bf82c-0932-4c0f-9c72-cd7283870f8d',
           "title": "Kontent Home",
           "github": "",
           "demo": "https://kontenthome.com/"
         },
+    
         {
-          "id": 2,
+          "id": 3,
           "image": 'https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/auth%20app.png?alt=media&token=e7068add-5595-4480-8eea-fa991e156783',
           "title": "Authentication app",
           "github": "https://github.com/Olu-Tobi/authapp",
@@ -17,7 +26,7 @@ export const Data = [
         },
       
         {
-          "id": 3,
+          "id": 4,
           "image": 'https://firebasestorage.googleapis.com/v0/b/shop-c8953.appspot.com/o/quillbox%20app.png?alt=media&token=77b06f04-e82e-4a0c-a19c-c275da5dafbe',
           "title": "Quillbox (Chat app)",
           "github": "https://github.com/Olu-Tobi/quillbox",
